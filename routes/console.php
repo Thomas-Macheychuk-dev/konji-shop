05d41494d37ef3d23c1ca7ee704c9c7f3be023f8
Tomasz Maciejczuk <tomasz.maciejczuk@proton.me>
2026-05-25 20:40:37 +0200

    Add Polkurier shipment status sync command

--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+
+Schedule::command('polkurier:sync-shipments --limit=50')
+    ->everyThirtyMinutes()
+    ->withoutOverlapping();
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
